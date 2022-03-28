c7a3f57e2355cdb0eaa9851e4f0e8d4166a56f3c
hamilamailee <hamilamailee@yahoo.com>
2022-03-29 00:20:07 +0430

    Use the config.yaml file for importing skeleton

--- convert.py
+++ convert.py
@@ -1,5 +1,10 @@
-import pandas as pd
+import yaml
 import numpy as np
+from asyncio.windows_events import NULL
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
 all_joints = []
 
 
@@ -8,6 +13,18 @@ class Joint:
         self.name = name
         self.connected_joints = []
         self.add_joint()
+
+    def get_joint(name):
+        for j in all_joints:
+            if j.name == name:
+                return j
+        return None
+
+    def connect_joints(bone) -> None:
+        j1 = Joint.get_joint(bone[0])
+        j2 = Joint.get_joint(bone[1])
+        j1.connected_joints.append(j2)
+        j2.connected_joints.append(j1)
 
     def get_cords_from_df(self, df) -> None:
         model = df[self.name]
@@ -19,17 +36,19 @@ class Joint:
     def add_joint(self) -> None:
         all_joints.append(self)
 
-    def connect_joints(self, joint) -> None:
-        self.connected_joints.append(joint)
-
     def print_joint(self) -> None:
         print("name : ", self.name)
         print("connected joints : ")
         for i in range(len(self.connected_joints)):
-            print(i.name)
+            print(self.connected_joints[i].name)
 
 
-file_path = "My Version\p1DLC_effnet_b0_Simba ModifiedAug27shuffle0_150000.csv"
+# file_path = input("Enter the path of your csv file: ")
+# conf_path = input("Enter the path of your config.yaml file: ")
+
+file_path = "p1DLC_effnet_b0_Simba ModifiedAug27shuffle0_150000.csv"
+conf_path = "config.yaml"
+
 df = pd.read_csv(file_path)
 df.columns = df[df['scorer'] == 'bodyparts'].to_numpy().tolist()
 df = df.drop([0, 1], axis=0)
@@ -40,3 +59,11 @@ for i in np.unique(list(df.columns)):
 
 for j in all_joints:
     j.get_cords_from_df(df)
+
+with open(conf_path) as file:
+    skeleton = yaml.load(file, Loader=yaml.FullLoader)['skeleton']
+    for bone in skeleton:
+        Joint.connect_joints(bone)
+
+for i in all_joints:
+    i.print_joint()
