09adad4abb82e7eef7b32ca399a201eb9c0560ee
Hamila Mailee <45606899+hamilamailee@users.noreply.github.com>
2022-04-16 00:56:28 +0430

    cleaned directory

---
+++ bvhtoolbox_script.py
@@ -0,0 +1,18 @@
+from bvhtoolbox import BvhNode, BvhTree
+import pandas as pd
+import numpy as np
+
+# file_path = input("Enter the path of your csv file: ")
+# conf_path = input("Enter the path of your config.yaml file: ")
+
+file_path = "p1DLC_effnet_b0_Simba ModifiedAug27shuffle0_150000.csv"
+conf_path = "config.yaml"
+
+df = pd.read_csv(file_path)
+df.columns = df[df['scorer'] == 'bodyparts'].to_numpy().tolist()
+df = df.drop([0, 1], axis=0)
+fc = len(df)
+
+for i in np.unique(list(df.columns)):
+    if i != "bodyparts":
+        BvhNode()
